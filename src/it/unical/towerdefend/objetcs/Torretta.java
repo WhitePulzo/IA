c8a612105d6a892bfa5c3f62fed43981113f3d0c
littleBoaR <dark.baron@hotmail.it>
2018-03-03 19:42:09 +0100

    aggiunti alcuni parametri a torretta, da implementare strategia di attacco

--- src/it/unical/towerdefend/objetcs/Torretta.java
+++ src/it/unical/towerdefend/objetcs/Torretta.java
@@ -3,6 +3,9 @@ package it.unical.towerdefend.objetcs;
 public class Torretta {
 	
 	private int IdTorre = 0; // le torrette, nelle loro specializzazioni hanno un id
+	public int prezzo;
+	public int prezzoDiVendita; // se rivendi una torretta ci guadagni di meno
+	Position posizioneTorre;
 	Mappa mappaDiGIoco;
 
 	public Torretta(Mappa m) {
@@ -10,21 +13,23 @@ public class Torretta {
 	}
 
 	public void attacca() {
-		// prende il primo mostro entrato nel suo range e lo attacca finchè non
-		// va via dall'arntea coperta
+		// la torretta valuta le posizioni che sono prossime ad essa, e le scorre con un for, appena trova un nemico, lo attacca ed esce dal for
+		// magari l'area di attacco è una sottomatrice 3x3 o 5x5, dobbiamo trovare il metodo giusto per implementare
 	}
 
+	// forse questa cosa è meglio farla ad un livello più alto
 	public void vendi() {
 		// vende la torretta aumentando di un tot il forziere dei coin del
 		// giocatore
 	}
-
+	
+	// forse questa cosa è meglio farla ad un livello più alto
 	public void compra() {
 		// acquista uno dei tre tipi di torrette
 	}
 
 	public void posiziona(Position p) {
-		// decide in quale posizione della mappa di gioco mettere la suddetta torretta
+		posizioneTorre = p;
 	}
 
 }
